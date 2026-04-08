d541be1d9b79af376270707af10c26bb2cefc2f3
xemlock <952555+xemlock@users.noreply.github.com>
2026-04-08 22:28:26 +0200

    feat: Try to resolve page in postDispatch if 404 error was thrown

--- library/ManiplePages/Controller/Plugin/PageResolver.php
+++ library/ManiplePages/Controller/Plugin/PageResolver.php
@@ -1,14 +1,12 @@
 <?php
 
 /**
- * Plugin which intercepts failed routing attempts and tries to redirect to
- * page view if requestUri matches any existing slug.
+ * Controller plugin which intercepts failed routing attempts and 404 dispatch errors
+ * and attempts redirect to page view if requestUri matches any existing slug.
  */
 class ManiplePages_Controller_Plugin_PageResolver extends Zend_Controller_Plugin_Abstract
 {
     const className = __CLASS__;
-
-    const REDIRECT_ROUTE = 'maniple-pages.pages.view';
 
     /**
      * @var bool
@@ -27,81 +25,125 @@ class ManiplePages_Controller_Plugin_PageResolver extends Zend_Controller_Plugin
 
     public function routeStartup(Zend_Controller_Request_Abstract $request)
     {
-        if (!$request instanceof Zend_Controller_Request_Http) {
+        $this->_captureExceptions();
+    }
+
+    public function routeShutdown(Zend_Controller_Request_Abstract $request)
+    {
+        $exception = $this->_restoreExceptions();
+
+        /** @var Zend_Controller_Router_Rewrite $router */
+        $router = Zend_Controller_Front::getInstance()->getRouter();
+
+        if (
+            ($exception || $router->getCurrentRouteName() === 'default')
+            && $this->_resolvePage($request)
+        ) {
+            $this->_clearExceptions();
             return;
         }
 
+        if ($exception && Zend_Controller_Front::getInstance()->throwExceptions()) {
+            throw $exception;
+        }
+    }
+
+    public function preDispatch(Zend_Controller_Request_Abstract $request)
+    {
+        if ($this->_resolvedPage) {
+            return;
+        }
+
+        $this->_captureExceptions();
+    }
+
+    public function postDispatch(Zend_Controller_Request_Abstract $request)
+    {
+        if ($this->_resolvedPage) {
+            return;
+        }
+
+        $exception = $this->_restoreExceptions();
+
+        if ($exception) {
+            if ($exception->getCode() === 404 && $this->_resolvePage($request)) {
+                $this->_clearExceptions();
+                return;
+            }
+            if (Zend_Controller_Front::getInstance()->throwExceptions()) {
+                throw $exception;
+            }
+        }
+    }
+
+    protected function _captureExceptions()
+    {
         $frontController = Zend_Controller_Front::getInstance();
         $this->_throwExceptions = $frontController->throwExceptions();
 
-        // suppress throwing routing exceptions, remember current number
-        // of exceptions already thrown - it will be used for determining
-        // if this plugin should throw during routeShutdown phase
+        // suppress throwing routing or dispatch exceptions, remember current number
+        // of exceptions already thrown, it will be used for determining if this plugin
+        // should throw last exception during routeShutdown or postDispatch phase
         $frontController->throwExceptions(false);
 
         $this->_exceptionCount = count($this->_response->getException());
     }
 
-    public function routeShutdown(Zend_Controller_Request_Abstract $request)
+    /**
+     * @return Exception|null
+     */
+    protected function _restoreExceptions()
     {
-        if (!$request instanceof Zend_Controller_Request_Http) {
-            return;
-        }
-
         $frontController = Zend_Controller_Front::getInstance();
         $frontController->throwExceptions($this->_throwExceptions);
-
-        /** @var Zend_Controller_Router_Rewrite $router */
-        $router = $frontController->getRouter();
 
         $exceptions = $this->_response->getException();
         $exceptionThrown = count($exceptions) - $this->_exceptionCount > 0;
 
-        if ($exceptionThrown || $router->getCurrentRouteName() === 'default') {
-            // route was not matched - try to check if any page slug matches
-            /** @var ManiplePages_Repository_PageRepository $pageRepository */
-            $pageRepository = $frontController
-                ->getParam('bootstrap')
-                ->getResource(ManiplePages_Repository_PageRepository::className);
+        return $exceptionThrown ? end($exceptions) : null;
+    }
 
-            $requestUri = $request->getRequestUri();
-            $baseUrl = $request->getBaseUrl();
+    protected function _clearExceptions()
+    {
+        // A little hacky, but there is no other way to clear exceptions from the response
+        $responseRef = new ReflectionObject($this->_response);
+        $responseRef->getProperty('_exceptions')->setValue($this->_response, []);
+    }
 
-            // remove baseUrl from requestUri
-            if (substr($requestUri, 0, strlen($baseUrl)) === $baseUrl) {
-                $requestUri = substr($requestUri, strlen($baseUrl));
-            }
+    protected function _resolvePage(Zend_Controller_Request_Abstract $request)
+    {
+        if (!$request instanceof Zend_Controller_Request_Http) {
+            return null;
+        }
 
-            $slug = trim(strtok($requestUri, '?'), '/');
-            $page = $pageRepository->getPageBySlug($slug);
+        // route was not matched - try to check if any page slug matches
+        /** @var ManiplePages_Repository_PageRepository $pageRepository */
+        $pageRepository = Zend_Controller_Front::getInstance()
+            ->getParam('bootstrap')
+            ->getResource(ManiplePages_Repository_PageRepository::className);
 
+        $requestUri = $request->getRequestUri();
+        $baseUrl = $request->getBaseUrl();
+
+        // remove baseUrl from requestUri
+        if (substr($requestUri, 0, strlen($baseUrl)) === $baseUrl) {
+            $requestUri = substr($requestUri, strlen($baseUrl));
+        }
+
+        $slug = trim(strtok($requestUri, '?'), '/');
+        $page = $pageRepository->getPageBySlug($slug);
+        $this->_resolvedPage = $page;
+
+        if ($page) {
+            $request->setModuleName('maniple-pages');
+            $request->setControllerName('pages');
+            $request->setActionName('view');
             $request->setParam('page', $page);
-            $this->_resolvedPage = $page;
-
-            if ($page) {
-                // during routeStartup/routeShutdown routing depends on requestUri
-                // rather on request's module/controller/action params
-                $routeUrl = $router->assemble(
-                    array('page_id' => $page->getId()),
-                    self::REDIRECT_ROUTE
-                );
-
-                $request->setRequestUri($routeUrl);
-                $request->setPathInfo(null);
-
-                // change router's currentRoute to page view
-                $router->route($request);
-
-                $request->setRequestUri($requestUri);
-                $request->setPathInfo(null);
-
-                return;
-            }
+            $request->setDispatched(false);
+            return true;
         }
 
-        if ($frontController->throwExceptions() && $exceptionThrown) {
-            throw end($exceptions);
-        }
+        return false;
     }
 
     /**
